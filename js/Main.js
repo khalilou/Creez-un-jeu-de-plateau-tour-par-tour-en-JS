11e11ba590ef9308aa43eee7032d9150a9b663be
khalil <khalil.al@hotmail.com>
2020-06-29 19:54:37 +0000

    add default weapon

--- js/Main.js
+++ js/Main.js
@@ -1,6 +1,11 @@
-$(document).ready(function() {
+$(document).ready(() => {
   //button new game
-  $( "#new-game" ).click(function() {
+  $( "#new-game" ).click(() => {
+    //initialize
+  $( "#playerOne-defend" ).off();
+  $( "#playerTwo-defend" ).off();
+  $( "#playerOne-attack" ).off();
+  $( "#playerTwo-attack" ).off();
     //make new instance of the Game object
     //Object player 1
     let playerOne = new Player("playerOne");
@@ -16,35 +21,34 @@ $(document).ready(function() {
         new Weapon('double_sword', 20)
     ];
     let startGame = new MapClass(10,10,playerOne,playerTwo, weapons);
-
     startGame.generateMap();
     startGame.checkPlayermoves(playerOne, playerTwo);
     //console.log(playerOne.turn);
 
    // player 1 fighting panel
   //button attack for player 1
-  $( "#playerOne-attack" ).click(function() {
-    startGame.playerOne.fight(startGame.playerTwo);
+  $( "#playerOne-attack" ).click(() => {
+    playerOne.fight(playerTwo);
   });
 
   //button defend for player 1
-  $( "#playerOne-defend" ).click(function() {
-    startGame.playerOne.fightingOption="defend";
-    startGame.playerOne.disableFight();
-    startGame.playerTwo.enableFight();
+  $( "#playerOne-defend" ).click(() => {
+    playerOne.fightingOption="defend";
+    playerOne.disableFight();
+    playerTwo.enableFight();
   });
 
   // player2 fighting panel
   //button attack for player 2
-  $( "#playerTwo-attack" ).click(function() {
-    startGame.playerTwo.fight(startGame.playerOne);
+  $( "#playerTwo-attack" ).click(() => {
+    playerTwo.fight(playerOne);
   });
 
   //button defend for player 2
-  $( "#playerTwo-defend" ).click(function() {
-    startGame.playerTwo.fightingOption="defend";
-    startGame.playerTwo.disableFight();
-    startGame.playerOne.enableFight();
+  $( "#playerTwo-defend" ).click(() => {
+    playerTwo.fightingOption="defend";
+    playerTwo.disableFight();
+    playerOne.enableFight();
   });
   
   });
